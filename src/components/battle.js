3309f51d1ed41be98bf9334b31712a477fc6386d
randyh <randyh@qualtrics.com>
2017-05-24 21:51:04 -0600

    update

--- src/components/battle.js
+++ src/components/battle.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 
 class Battle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null
+    }
+    this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    this.setState(function (id, username) {
+      var newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] = 'https://github.com' + username + '.png?size=200';
+    });
+  }
   render () {
     return (
       <div>
